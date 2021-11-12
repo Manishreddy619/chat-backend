46d26b088dc2886b382768c8fc865ac59a84335a
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-11-12 18:16:53 +0100

    add files

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const http = require('http');
-app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use(router);
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/uploads'));
 
-const port = config.PORT || 3001;
+const port = config.appPort;
 
 const server = http.createServer(app);
 const SocketServer = require('./socket');
