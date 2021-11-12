02f97660daf1fdf6e5a91ecd8cd60af85561bd11
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-11-12 18:25:39 +0100

    add files

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(router);
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/uploads'));
 
-const port = config.appPort;
+const port = 3030;
 
 const server = http.createServer(app);
 const SocketServer = require('./socket');
